53872505702a48b6f20f3622004d39eb47dcf878
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2023-08-10 10:10:19 +0300

    checking seq

--- seq.c
+++ seq.c
@@ -1,146 +1,230 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define MAX_POINTS 100000
 
-typedef struct {
-    int x1;
-    int x2;
-    int a;
-    int b;
-} PointParams;
-
-typedef struct {
-    int x;
-    int y;
+typedef struct
+{
     int id;
+    double x1, x2, a, b;
 } Point;
 
-void readInputFile(const char* filename, int* N, int* K, double* D, int* TCount, PointParams** params) {
-    FILE* file = fopen(filename, "r");
-    if (file == NULL) {
+typedef struct
+{
+    int id;
+    double x, y;
+    int t;
+} Coordinate;
+
+void calculateCoordinates(int N, int TCount, Point points[], Coordinate coordinates[][N])
+{
+    double t;
+    int id = 1;
+    for (int i = 0; i < TCount; i++)
+    {
+        t = 2.0 * i / TCount - 1.0;
+
+        for (int j = 0; j < N; j++)
+        {
+            double x = ((points[j].x2 - points[j].x1) / 2.0) * sin(t * M_PI / 2.0) + (points[j].x2 + points[j].x1) / 2.0;
+            double y = points[j].a * x + points[j].b;
+
+            coordinates[i][j].id = id;
+            coordinates[i][j].x = x;
+            coordinates[i][j].y = y;
+            id++;
+        }
+    }
+}
+
+void readInputFile(const char *filename, int *N, int *K, float *D, int *TCount, Point points[])
+{
+    FILE *inputFile = fopen(filename, "r");
+    if (inputFile == NULL)
+    {
         printf("Failed to open the input file.\n");
-        return;
+        exit(1);
     }
 
-    fscanf(file, "%d %d %lf %d\n", N, K, D, TCount);
-
-    *params = (PointParams*)malloc((*N) * sizeof(PointParams));
-
-    for (int i = 0; i < *N; i++) {
-        fscanf(file, "%d %d %d %d\n", &((*params)[i].x1), &((*params)[i].x2), &((*params)[i].a), &((*params)[i].b));
+    // Read the input parameters
+    int result = fscanf(inputFile, "%d %d %f %d", N, K, D, TCount);
+    if (result != 4)
+    {
+        printf("Error reading input parameters from the file.\n");
+        fclose(inputFile);
+        exit(1);
     }
 
-    fclose(file);
-}
-
-void writeOutputFile(const char* filename, double* tValues, int** pointIDs, int resultCount) {
-    FILE* file = fopen(filename, "w");
-    if (file == NULL) {
-        printf("Failed to open the output file.\n");
-        return;
+    int size = *N;
+    if (size <= 0 || size > MAX_POINTS)
+    {
+        printf("Invalid value for N in the input file.\n");
+        fclose(inputFile);
+        exit(1);
     }
 
-    if (resultCount > 0) {
-        for (int i = 0; i < resultCount; i++) {
-            fprintf(file, "Points  %d, %d, %d satisfy Proximity Criteria at t = %lf\n",
-                pointIDs[i][0], pointIDs[i][1], pointIDs[i][2], tValues[i]);
-        }
-    } else {
-        fprintf(file, "There were no 3 points found for any t.\n");
-    }
-
-    fclose(file);
-}
-
-double calculateX(int x1, int x2, double t) {
-    return ((x2 - x1) / 2.0) * sin(t * M_PI / 2.0) + (x2 + x1) / 2.0;
-}
-
-int checkProximityCriteria(PointParams params, Point p, double D, int N, Point* points, int K) {
-    int count = 0;
-    for (int i = 0; i < N; i++) {
-        if (i != p.id) {
-            double distance = sqrt(pow(p.x - points[i].x, 2) + pow(p.y - points[i].y, 2));
-            if (distance < D)
-                count++;
-            if (count >= K)
-                return 1;
+    // Read the points data
+    for (int i = 0; i < size; i++)
+    {
+        result = fscanf(inputFile, "%d %lf %lf %lf %lf", &points[i].id, &points[i].x1, &points[i].x2, &points[i].a, &points[i].b);
+        if (result != 5)
+        {
+            printf("Error reading data for Point %d from the file.\n", i + 1);
+            fclose(inputFile);
+            exit(1);
         }
     }
-    return 0;
+
+    fclose(inputFile);
 }
 
-void findSatisfyingPoints(int N, PointParams* params, int K, double D, int TCount, Point* points, int* resultCount, double** tValues, int*** pointIDs) {
-    *resultCount = 0;
-    *pointIDs = NULL;
-    *tValues = (double*)malloc(TCount * sizeof(double));
+void printCoordinates(int N, int TCount, Coordinate coordinates[][N])
+{
+    for (int i = 0; i < TCount; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("Coordinate ID: %d, x: %.2f, y: %.2f\n", coordinates[i][j].id, coordinates[i][j].x, coordinates[i][j].y);
+        }
+    }
+}
 
-    for (int i = 0; i <= TCount; i++) {
-        double t = 2.0 * i / TCount - 1.0;
-        (*tValues)[i] = t;
+int checkDistanceSmallerThanD(double x1, double y1, double x2, double y2, float D)
+{
+    double distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    return (distance < D) ? 1 : 0;
+}
 
-        for (int j = 0; j < N - 2; j++) {
-            for (int k = j + 1; k < N - 1; k++) {
-                for (int l = k + 1; l < N; l++) {
-                    Point p1, p2, p3;
-                    p1.id = j;
-                    p2.id = k;
-                    p3.id = l;
+int **CheckRowsCoordinates(int N, float D, Coordinate coordinate[][N], int TCount, int K)
+{
+    int currentCol = 0;
+    int currentRow = 0;
+    int KCount = 0;
+    int pointCount = 0;                                    
+    int **idMatrix = (int **)malloc(TCount * sizeof(int *)); // Allocate memory for all rows at once
 
-                    p1.x = calculateX(params[j].x1, params[j].x2, t);
-                    p2.x = calculateX(params[k].x1, params[k].x2, t);
-                    p3.x = calculateX(params[l].x1, params[l].x2, t);
+    // Initialize idMatrix with all zeroes using calloc
+    for (int i = 0; i < TCount; i++)
+    {
+        idMatrix[i] = (int *)calloc(3, sizeof(int));
+    }
 
-                    p1.y = params[j].a * p1.x + params[j].b;
-                    p2.y = params[k].a * p2.x + params[k].b;
-                    p3.y = params[l].a * p3.x + params[l].b;
+    while (currentRow < TCount)
+    {
+        Coordinate temp = coordinate[currentRow][currentCol];
 
-                    if (checkProximityCriteria(params[j], p1, D, N, points, K) &&
-                        checkProximityCriteria(params[k], p2, D, N, points, K) &&
-                        checkProximityCriteria(params[l], p3, D, N, points, K)) {
-                        (*resultCount)++;
-                        *pointIDs = (int**)realloc(*pointIDs, (*resultCount) * sizeof(int*));
-                        (*pointIDs)[(*resultCount) - 1] = (int*)malloc(3 * sizeof(int));
-                        (*pointIDs)[(*resultCount) - 1][0] = p1.id;
-                        (*pointIDs)[(*resultCount) - 1][1] = p2.id;
-                        (*pointIDs)[(*resultCount) - 1][2] = p3.id;
-                        break;
-                    }
-                }
-                if (*resultCount > 0) break;
+        // Loops through the point and checks if it's valid
+        for (int i = 0; i < N; i++)
+        {
+            if (currentCol != i)
+            {
+                Coordinate currentCord = coordinate[currentRow][i];
+                KCount += checkDistanceSmallerThanD(temp.x, temp.y, currentCord.x, currentCord.y, D);
             }
-            if (*resultCount > 0) break;
+            if (KCount == K)
+            {
+                pointCount++;
+                // Store the point ID in the array
+                idMatrix[currentRow][pointCount] = temp.id;
+                // Reset KCount for the next point
+                KCount = 0;
+            }
         }
+
+        currentCol++;
+        if (currentCol == N)
+        {
+            // Move to the next row
+            currentRow++;
+            currentCol = 0;
+            pointCount = 0; // Reset pointCount for the new row
+        }
+    }
+
+    return idMatrix;
+}
+
+void printInputValues(int N, int K, float D, int TCount, Point points[])
+{
+    printf("N: %d\n", N);
+    printf("K: %d\n", K);
+    printf("D: %.2f\n", D);
+    printf("TCount: %d\n", TCount);
+
+    printf("Points:\n");
+    for (int i = 0; i < N; i++)
+    {
+        printf("ID: %d, x1: %.2f, x2: %.2f, a: %.2f, b: %.2f\n",
+               points[i].id, points[i].x1, points[i].x2, points[i].a, points[i].b);
     }
 }
 
-int main() {
+void WriteToOutputFile(const char *filename, int **idMatrix, int TCount)
+{
+    FILE *outputFile = fopen(filename, "w");
+    if (outputFile == NULL)
+    {
+        printf("Error opening output file: %s\n", filename);
+        return;
+    }
+    int count = 0;
+
+    for (int i = 0; i < TCount; i++)
+    {
+        int id1 = idMatrix[i][0];
+        int id2 = idMatrix[i][1];
+        int id3 = idMatrix[i][2];
+
+        if (id1 > 0 && id2 > 0 && id3 > 0)
+        {
+            count++;
+            fprintf(outputFile, "Points %d, %d, %d satisfy Proximity Criteria at t = t%d\n", id1, id2, id3, i + 1);
+        }
+    }
+    if (count == 0)
+    {
+        fprintf(outputFile, "Aren't any points");
+    }
+
+    fclose(outputFile);
+}
+void PrintMatrix(int **matrix, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+
     int N, K, TCount;
-    double D;
-    PointParams* params;
-    Point* points;
-    int resultCount;
-    double* tValues;
-    int** pointIDs;
+    float D;
+    Point points[MAX_POINTS];
 
-    readInputFile("input.txt", &N, &K, &D, &TCount, &params);
+    readInputFile("input.txt", &N, &K, &D, &TCount, points);
+    Coordinate coordinates[TCount][N];
+    // Print the read values
 
-    points = (Point*)malloc(N * sizeof(Point));
-    for (int i = 0; i < N; i++) {
-        points[i].id = i;
+    calculateCoordinates(N, TCount, points, coordinates);
+
+    printCoordinates(N, TCount, coordinates);
+
+    int **idMatrix = CheckRowsCoordinates(N, D, coordinates, TCount, K);
+    PrintMatrix(idMatrix, TCount, 3);
+    WriteToOutputFile("output.txt", idMatrix, TCount);
+
+    // Don't forget to free the memory allocated for idMatrix
+    for (int i = 0; i < TCount; i++)
+    {
+        free(idMatrix[i]);
     }
-
-    findSatisfyingPoints(N, params, K, D, TCount, points, &resultCount, &tValues, &pointIDs);
-
-    writeOutputFile("output.txt", tValues, pointIDs, resultCount);
-
-    free(params);
-    free(points);
-    free(tValues);
-    for (int i = 0; i < resultCount; i++) {
-        free(pointIDs[i]);
-    }
-    free(pointIDs);
+    free(idMatrix);
 
     return 0;
 }
